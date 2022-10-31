e563ebfc79230f077833939b8b6740e00d9db968
jim2764 <116091392+jim2764@users.noreply.github.com>
2022-10-31 08:39:57 +0800

    Update GuessNumber.cs

--- Q1/GuessNumber.cs
+++ Q1/GuessNumber.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Q1
 {
-	public class GuessNumber
+    public class GuessNumber
     {
         private string answer;
         private string show;
@@ -12,7 +12,7 @@ namespace Q1
         private int[] guessInformation;
 
 
-		public string Answer { get { return answer; } }
+	public string Answer { get { return answer; } }
 
         public void NewGame()
         {
@@ -25,7 +25,7 @@ namespace Q1
             // Get answer information
             GetInformation(answerInformation, answer);
             show = String.Empty;
-		}
+	}
 
         public string Guess(string input)
         {
@@ -34,13 +34,12 @@ namespace Q1
 
             guessInformation = new int[10];
             // Array Initialization
-			ArrayInitialization(guessInformation);
-
-			// Get input information
-			GetInformation(guessInformation, input);
+            ArrayInitialization(guessInformation);
+            // Get input information
+            GetInformation(guessInformation, input);
 
             // Determine XAXB
-			int counterA = 0;
+            int counterA = 0;
             int counterB = 0;
             for (int i = 0; i < answerInformation.Length; i++)
             {
@@ -52,10 +51,9 @@ namespace Q1
                         counterB++;
                 }
             }
-
-			show += $"{input} {counterA}A{counterB}B\r\n";
-
-			return show;
+            show += $"{input} {counterA}A{counterB}B\r\n";
+                
+            return show;
         }
 
         private void GetInformation(int[] information, string value)
@@ -66,7 +64,7 @@ namespace Q1
                 int integer = (int)char.GetNumericValue(value[i]);
                 information[integer] = i;
             }
-		}
+        }
 
         private string GetNumber()
         {
@@ -81,13 +79,13 @@ namespace Q1
             for (int i = 0; i < numbers.Length; i++)
             {
                 // Get random number
-				int seed = Guid.NewGuid().GetHashCode();
-				Random random = new Random(seed);
+		int seed = Guid.NewGuid().GetHashCode();
+		Random random = new Random(seed);
                 int randomNumber = random.Next(0, 10);
 
                 // swap 
                 Swap(ref numbers[i], ref numbers[randomNumber]);
-			}
+	    }
 
             // return first 4 number
             string result = string.Empty;
@@ -97,19 +95,19 @@ namespace Q1
             }
             return result;
         }
-
-		private string IsRight(string value)
-		{
+	
+	private string IsRight(string value)
+	{
             // Is less than four
-			if (value.Length != 4) throw new Exception("Please input 4-digit number!!");
-
+            if (value.Length != 4) throw new Exception("Please input 4-digit number!!");
+              
             // Is not number
-			for (int i = 0; i < value.Length; i++)
-			{
-				int integer = (int)char.GetNumericValue(value[i]);
-				if (integer < 0 || integer > 9) throw new Exception("Please Enter number");
-			}
-
+            for (int i = 0; i < value.Length; i++)
+            {
+                int integer = (int)char.GetNumericValue(value[i]);
+		if (integer < 0 || integer > 9) throw new Exception("Please Enter number");
+            }
+               
             // Is repeat number
             HashSet<char> chars = new HashSet<char>();
             for(int i = 0; i < value.Length; i++)
@@ -117,9 +115,9 @@ namespace Q1
                 chars.Add(value[i]);
             }
             if (chars.Count < 4) throw new Exception("Please enter different number");
-
-			return value;
-		}
+                
+            return value;
+	}
 
         private void ArrayInitialization(int[] information)
         {
@@ -127,7 +125,7 @@ namespace Q1
                 information[i] = -1;
         }
 
-		private void Swap(ref int number1, ref int number2)
+	private void Swap(ref int number1, ref int number2)
         {
             int temp = number1;
             number1 = number2;
